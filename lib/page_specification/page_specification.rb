55eabd08633ef3a8064d41527d6e9dc57baaa164
Mauricio Massaaki Yochizumi <mauricio.massaaki@gmail.com>
2016-11-15 09:37:38 -0200

    Added word count

--- lib/page_specification/page_specification.rb
+++ lib/page_specification/page_specification.rb
@@ -1,5 +1,18 @@
 module PageSpecification
 	def self.count_words(text)
-		text.size
+		words = text.split(" ")
+		words.count
+	end
+
+	def self.count_world(text, string)
+		worlds = text.split(" ")
+		count = 0
+
+		worlds.each do |w|
+			if (w == string) 
+				count += 1
+			end
+		end
+		count
 	end
 end
